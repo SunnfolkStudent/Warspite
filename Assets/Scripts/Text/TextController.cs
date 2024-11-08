f0a1454a842e15e1ff58e7726f8b2a639c988130
Magnus <magnus.nistad.helvig@gmail.com>
2024-11-08 11:59:48 +0100

    Dummy

--- Assets/Scripts/Text/TextController.cs
+++ Assets/Scripts/Text/TextController.cs
@@ -1,10 +1,16 @@
+using System;
 using UnityEngine;
 using TMPro;
 
 public class TextController : MonoBehaviour
 {
     public TMP_Text text;
-   
+
+    private void Start()
+    {
+        text.gameObject.SetActive(false);
+    }
+
     void OnTriggerEnter2D(Collider2D coll)
     {
         text.gameObject.SetActive(true);
